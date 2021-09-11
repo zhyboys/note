0b4477cc45ca732b0704f4554e6df6abef66b599
赵桓熠 <2625402133@qq.com>
2021-09-11 19:27:51 +0800

    node完结

--- docs/typora/00前端系列/16node三大框架/sidebar.js
+++ docs/typora/00前端系列/16node三大框架/sidebar.js
@@ -16,6 +16,22 @@ module.exports =
                 '/typora/00前端系列/16node三大框架/009连接数据库报错解决.md',
                 '/typora/00前端系列/16node三大框架/010显示登录注册界面.md',
                 '/typora/00前端系列/16node三大框架/011前后端数据校验.md',
+                '/typora/00前端系列/16node三大框架/012后端架构分层处理.md',
+                '/typora/00前端系列/16node三大框架/013用户注册.md',
+                '/typora/00前端系列/16node三大框架/014密码加密.md',
+                '/typora/00前端系列/16node三大框架/015用户登录.md',
+                '/typora/00前端系列/16node三大框架/016客户端保存登录状态.md',
+                '/typora/00前端系列/16node三大框架/017客户端保存登录安全问题.md',
+                '/typora/00前端系列/16node三大框架/018服务端保存登录状态.md',
+                '/typora/00前端系列/16node三大框架/019全局变量保存登录状态问题.md',
+                '/typora/00前端系列/16node三大框架/020Redis工具类封装.md',
+                '/typora/00前端系列/16node三大框架/021Redis存储登录状态.md',
+                '/typora/00前端系列/16node三大框架/022Sql注入.md',
+                '/typora/00前端系列/16node三大框架/023Sequelize替换MySQL.md',
+                '/typora/00前端系列/16node三大框架/024Sequelize注册登录.md',
+                '/typora/00前端系列/16node三大框架/025日志记录.md',
+                '/typora/00前端系列/16node三大框架/026日志分析.md',
+                '/typora/00前端系列/16node三大框架/027插件与工具类总结.md',
             ]
         }
     ]
