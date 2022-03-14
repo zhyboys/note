51b9f60a2ca9ed26d2e250d6eecf190bae3f1208
hyzhao <hyzhao@csih.cn>
2022-03-14 14:08:05 +0800

    Signed-off-by: hyzhao <hyzhao@csih.cn>

--- docs/.vuepress/sidebar.js
+++ docs/.vuepress/sidebar.js
@@ -1,47 +1,47 @@
 module.exports=
 {	
 	// 注意点：:require前面的字符串不能为英文否则报错！！
-	'/typora/00guide/':require('../typora/00guide/sidebar'),
+	'/typora/00guide/':require('./sidebar/00guide/sidebar'),
 
 	/**
 	 * 前端系列栏
 	 */
-	'/typora/01frontEnd/00summary/':require('../typora/01frontEnd/00summary/sidebar'),
-	'/typora/01frontEnd/02JavaScript/':require('../typora/01frontEnd/02JavaScript/sidebar'),
-	'/typora/01frontEnd/05JavaScriptNewFeature/':require('../typora/01frontEnd/05JavaScriptNewFeature/sidebar'),
-	'/typora/01frontEnd/03CSSPreprocessor/01Less/':require('../typora/01frontEnd/03CSSPreprocessor/01Less/sidebar'),
-	'/typora/01frontEnd/03CSSPreprocessor/02fullPage/':require('../typora/01frontEnd/03CSSPreprocessor/02fullPage/sidebar'),
-	'/typora/01frontEnd/03CSSPreprocessor/03SASS/':require('../typora/01frontEnd/03CSSPreprocessor/03SASS/sidebar'),
-	'/typora/01frontEnd/04jQueryAndAjax/01jQuery/':require('../typora/01frontEnd/04jQueryAndAjax/01jQuery/sidebar'),
-	'/typora/01frontEnd/04jQueryAndAjax/02Ajax/':require('../typora/01frontEnd/04jQueryAndAjax/02Ajax/sidebar'),
-	'/typora/01frontEnd/07webAppDevelop/':require('../typora/01frontEnd/07webAppDevelop/sidebar'),
-	'/typora/01frontEnd/08Git/':require('../typora/01frontEnd/08Git/sidebar'),
-	'/typora/01frontEnd/09NodeJS/':require('../typora/01frontEnd/09NodeJS/sidebar'),
-	'/typora/01frontEnd/10Webpack/':require('../typora/01frontEnd/10Webpack/sidebar'),
-	'/typora/01frontEnd/11Vue/':require('../typora/01frontEnd/11Vue/sidebar'),
-	'/typora/01frontEnd/12VueCode/':require('../typora/01frontEnd/12VueCode/sidebar'),
+	'/typora/01frontEnd/00summary/':require('./sidebar/01frontEnd/00summary'),
+	'/typora/01frontEnd/02JavaScript/':require('./sidebar/01frontEnd/02JavaScript'),
+	'/typora/01frontEnd/05JavaScriptNewFeature/':require('./sidebar/01frontEnd/05JavaScriptNewFeature'),
+	'/typora/01frontEnd/03CSSPreprocessor/01Less/':require('./sidebar/01frontEnd/03CSSPreprocessor/01Less'),
+	'/typora/01frontEnd/03CSSPreprocessor/02fullPage/':require('./sidebar/01frontEnd/03CSSPreprocessor/02fullPage'),
+	'/typora/01frontEnd/03CSSPreprocessor/03SASS/':require('./sidebar/01frontEnd/03CSSPreprocessor/03SASS'),
+	'/typora/01frontEnd/04jQueryAndAjax/01jQuery/':require('./sidebar/01frontEnd/04jQueryAndAjax/01jQuery'),
+	'/typora/01frontEnd/04jQueryAndAjax/02Ajax/':require('./sidebar/01frontEnd/04jQueryAndAjax/02Ajax'),
+	'/typora/01frontEnd/07webAppDevelop/':require('./sidebar/01frontEnd/07webAppDevelop'),
+	'/typora/01frontEnd/08Git/':require('./sidebar/01frontEnd/08Git'),
+	'/typora/01frontEnd/09NodeJS/':require('./sidebar/01frontEnd/09NodeJS'),
+	'/typora/01frontEnd/10Webpack/':require('./sidebar/01frontEnd/10Webpack'),
+	'/typora/01frontEnd/11Vue/':require('./sidebar/01frontEnd/11Vue'),
+	'/typora/01frontEnd/12VueCode/':require('./sidebar/01frontEnd/12VueCode'),
 
 	/**
 	 * node后端
 	 */
-	 '/typora/01frontEnd/13MySQL/':require('../typora/01frontEnd/13MySQL/sidebar'),
-	 '/typora/01frontEnd/16nodeFramework/':require('../typora/01frontEnd/16nodeFramework/sidebar'),
-	 '/typora/01frontEnd/18TypeScript/':require('../typora/01frontEnd/18TypeScript/sidebar'),
-	 '/typora/01frontEnd/50Python/':require('../typora/01frontEnd/50Python/sidebar'),
-	 '/typora/01frontEnd/51PythonAutomatedTesting/':require('./sidebar/51PythonAutomatedTesting'),
+	 '/typora/01frontEnd/13MySQL/':require('./sidebar/01frontEnd/13MySQL'),
+	 '/typora/01frontEnd/16nodeFramework/':require('./sidebar/01frontEnd/16nodeFramework'),
+	 '/typora/01frontEnd/18TypeScript/':require('./sidebar/01frontEnd/18TypeScript'),
+	 '/typora/01frontEnd/50Python/':require('./sidebar/01frontEnd/50Python'),
+	 '/typora/01frontEnd/51PythonAutomatedTesting/':require('./sidebar/01frontEnd/51PythonAutomatedTesting'),
 	/**
 	 * 废弃discard
 	 */
-	'/typora/05discard/01MySQL/':require('../typora/05discard/01MySQL/sidebar'),
-	'/typora/05discard/02HTML+css/':require('../typora/05discard/02HTML+css/sidebar'),
-	'/typora/05discard/03CProgramming/':require('../typora/05discard/03CProgramming/sidebar'),
-	'/typora/05discard/04C++/':require('../typora/05discard/04C++/sidebar'),
-	'/typora/05discard/05JavaScript/':require('../typora/05discard/05JavaScript/sidebar'),
-	'/typora/05discard/06softExam/':require('../typora/05discard/06softExam/sidebar'),
-	'/typora/05discard/07Java1/':require('../typora/05discard/07Java1/sidebar'),
-	'/typora/05discard/08Java2/':require('../typora/05discard/08Java2/sidebar'),
-	'/typora/05discard/09Javaweb/':require('../typora/05discard/09Javaweb/sidebar'),
-	'/typora/05discard/10JDBC/':require('../typora/05discard/10JDBC/sidebar'),
-	'/typora/05discard/11JSP/':require('../typora/05discard/11JSP/sidebar'),
-	'/typora/05discard/12Auto/':require('../typora/05discard/12Auto/sidebar')
+	'/typora/05discard/01MySQL/':require('./sidebar/05discard/01MySQL'),
+	'/typora/05discard/02HTML+css/':require('./sidebar/05discard/02HTML+css'),
+	'/typora/05discard/03CProgramming/':require('./sidebar/05discard/03CProgramming'),
+	'/typora/05discard/04C++/':require('./sidebar/05discard/04C++'),
+	'/typora/05discard/05JavaScript/':require('./sidebar/05discard/05JavaScript'),
+	'/typora/05discard/06softExam/':require('./sidebar/05discard/06softExam'),
+	'/typora/05discard/07Java1/':require('./sidebar/05discard/07Java1'),
+	'/typora/05discard/08Java2/':require('./sidebar/05discard/08Java2'),
+	'/typora/05discard/09Javaweb/':require('./sidebar/05discard/09Javaweb'),
+	'/typora/05discard/10JDBC/':require('./sidebar/05discard/10JDBC'),
+	'/typora/05discard/11JSP/':require('./sidebar/05discard/11JSP'),
+	'/typora/05discard/12Auto/':require('./sidebar/05discard/12Auto')
 }
---
+++ docs/.vuepress/sidebar/01frontEnd/05JavaScriptNewFeature.js
@@ -0,0 +1,189 @@
+module.exports =
+    [
+        {
+            text: "1:网络编程",
+            collapsable:true,
+            children:
+            [
+                '/typora/01frontEnd/05JavaScriptNewFeature/001H5新增存储.md',
+                '/typora/01frontEnd/05JavaScriptNewFeature/002同源策略.md',
+                '/typora/01frontEnd/05JavaScriptNewFeature/003JSONP.md',
+                '/typora/01frontEnd/05JavaScriptNewFeature/004jQuery中JSONP.md',
+                '/typora/01frontEnd/05JavaScriptNewFeature/005JSONP封装.md',
+                '/typora/01frontEnd/05JavaScriptNewFeature/006JSONP小练习.md',
+                '/typora/01frontEnd/05JavaScriptNewFeature/007常识补充.md',
+                '/typora/01frontEnd/05JavaScriptNewFeature/008promise概念.md',
+                '/typora/01frontEnd/05JavaScriptNewFeature/009promise基础.md',
+                '/typora/01frontEnd/05JavaScriptNewFeature/010promise之then.md',
+                '/typora/01frontEnd/05JavaScriptNewFeature/011promise之catch.md',
+                '/typora/01frontEnd/05JavaScriptNewFeature/012异常处理机制.md',
+                '/typora/01frontEnd/05JavaScriptNewFeature/013手撕Promise上.md',
+                '/typora/01frontEnd/05JavaScriptNewFeature/014手撕Promise中.md',
+                '/typora/01frontEnd/05JavaScriptNewFeature/015手撕Promise下.md',
+                '/typora/01frontEnd/05JavaScriptNewFeature/016promise练习.md',
+                '/typora/01frontEnd/05JavaScriptNewFeature/017promise中all方法.md',
+                '/typora/01frontEnd/05JavaScriptNewFeature/018promise中all实现.md',
+                '/typora/01frontEnd/05JavaScriptNewFeature/019promise中race方法.md',
+                '/typora/01frontEnd/05JavaScriptNewFeature/020promise中race方法实现.md',
+                '/typora/01frontEnd/05JavaScriptNewFeature/021promise改造Ajax.md',
+                '/typora/01frontEnd/05JavaScriptNewFeature/022fetch基本使用.md',
+                '/typora/01frontEnd/05JavaScriptNewFeature/023fetch简单封装.md',
+                '/typora/01frontEnd/05JavaScriptNewFeature/024axios网络请求库.md',
+                '/typora/01frontEnd/05JavaScriptNewFeature/025Symbol类型.md',
+                '/typora/01frontEnd/05JavaScriptNewFeature/026Symbol注意点.md',
+                '/typora/01frontEnd/05JavaScriptNewFeature/027Iterator基本概念.md',
+                '/typora/01frontEnd/05JavaScriptNewFeature/028Iterator应用场景.md',
+                '/typora/01frontEnd/05JavaScriptNewFeature/029Generator函数.md',
+                '/typora/01frontEnd/05JavaScriptNewFeature/030Generator函数应用场景1.md',
+                '/typora/01frontEnd/05JavaScriptNewFeature/031Generator函数应用场景2.md',
+                '/typora/01frontEnd/05JavaScriptNewFeature/032Generator函数应用场景3.md',
+                '/typora/01frontEnd/05JavaScriptNewFeature/033async函数.md',
+                '/typora/01frontEnd/05JavaScriptNewFeature/034备用1.md',
+                '/typora/01frontEnd/05JavaScriptNewFeature/035备用2.md',
+            
+            ]
+        },
+        {
+            text: "2:Zepto",
+            collapsable: true,
+            children: 
+            [
+                '/typora/01frontEnd/05JavaScriptNewFeature/036zepto初体验.md',
+                '/typora/01frontEnd/05JavaScriptNewFeature/037zepto选择器.md',
+                '/typora/01frontEnd/05JavaScriptNewFeature/038zepto动画.md',
+                '/typora/01frontEnd/05JavaScriptNewFeature/039zepto-tap事件.md',
+                '/typora/01frontEnd/05JavaScriptNewFeature/040移动端Touch事件.md',
+                '/typora/01frontEnd/05JavaScriptNewFeature/041移动端Touch事件对象.md',
+                '/typora/01frontEnd/05JavaScriptNewFeature/042移动端Touch事件位置.md',
+                '/typora/01frontEnd/05JavaScriptNewFeature/043移动端手指位置练习.md',
+                '/typora/01frontEnd/05JavaScriptNewFeature/044zepto-tap事件原理.md',
+                '/typora/01frontEnd/05JavaScriptNewFeature/045移动端点透问题.md',
+                '/typora/01frontEnd/05JavaScriptNewFeature/046移动端点透问题解决.md',
+                '/typora/01frontEnd/05JavaScriptNewFeature/047zepto滑动事件.md',
+                '/typora/01frontEnd/05JavaScriptNewFeature/048滑动事件练习.md',
+                '/typora/01frontEnd/05JavaScriptNewFeature/049滑动事件示例.md',
+            ]
+        },
+        {
+            text: "3:IScorll",
+            collapsable: true,
+            children:
+            [
+                '/typora/01frontEnd/05JavaScriptNewFeature/050侧边栏菜单.md',
+                '/typora/01frontEnd/05JavaScriptNewFeature/051iScroll基本使用.md',
+                '/typora/01frontEnd/05JavaScriptNewFeature/052iScroll高级使用.md',
+                '/typora/01frontEnd/05JavaScriptNewFeature/053侧边栏菜单.md',
+
+            ]
+        },
+        {
+            text: "4:Swiper",
+            collapsable: true,
+            children:
+            [
+                '/typora/01frontEnd/05JavaScriptNewFeature/054移动端轮播图.md',
+                '/typora/01frontEnd/05JavaScriptNewFeature/055swiper基本使用.md',
+                '/typora/01frontEnd/05JavaScriptNewFeature/056swiper高级使用.md',
+                '/typora/01frontEnd/05JavaScriptNewFeature/057移动端轮播图.md',
+                '/typora/01frontEnd/05JavaScriptNewFeature/058swiper-animation.md',
+                '/typora/01frontEnd/05JavaScriptNewFeature/059酷狗首页.md',
+
+            ]
+        },
+        {
+            text: "5:动画插件 浅学",
+            collapsable: true,
+            children:
+            [
+                '/typora/01frontEnd/05JavaScriptNewFeature/060animateCSS.md',
+                '/typora/01frontEnd/05JavaScriptNewFeature/061WOWJS使用.md',
+                '/typora/01frontEnd/05JavaScriptNewFeature/062WOWJS位置计算.md',
+                '/typora/01frontEnd/05JavaScriptNewFeature/063WOWJS高级使用.md',
+                '/typora/01frontEnd/05JavaScriptNewFeature/064ScrollRevealJs.md',
+            ]
+        },
+        {
+            text: "6:Canvas",
+            collapsable: true,
+            children:
+            [
+                '/typora/01frontEnd/05JavaScriptNewFeature/065canvas所有方法.md',
+                '/typora/01frontEnd/05JavaScriptNewFeature/066canvas开篇.md',
+                '/typora/01frontEnd/05JavaScriptNewFeature/067canvas注意点.md',
+                '/typora/01frontEnd/05JavaScriptNewFeature/068canvas多根线条.md',
+                '/typora/01frontEnd/05JavaScriptNewFeature/069canvas简单图形.md',
+                '/typora/01frontEnd/05JavaScriptNewFeature/070canvas填充图形.md',
+                '/typora/01frontEnd/05JavaScriptNewFeature/071canvas虚线.md',
+                '/typora/01frontEnd/05JavaScriptNewFeature/072canvas绘制表格.md',
+                '/typora/01frontEnd/05JavaScriptNewFeature/073canvas绘制坐标系.md',
+                '/typora/01frontEnd/05JavaScriptNewFeature/074canvas绘制数据点.md',
+                '/typora/01frontEnd/05JavaScriptNewFeature/075canvas绘制折线图.md',
+                '/typora/01frontEnd/05JavaScriptNewFeature/076canvas折线图封装.md',
+                '/typora/01frontEnd/05JavaScriptNewFeature/077canvas绘制矩形.md',
+                '/typora/01frontEnd/05JavaScriptNewFeature/078canvas绘制柱状图.md',
+                '/typora/01frontEnd/05JavaScriptNewFeature/079canvas渐变色.md',
+                '/typora/01frontEnd/05JavaScriptNewFeature/080canvas绘制圆弧.md',
+                '/typora/01frontEnd/05JavaScriptNewFeature/081canvas绘制扇形.md',
+                '/typora/01frontEnd/05JavaScriptNewFeature/082canvas绘制饼状图.md',
+                '/typora/01frontEnd/05JavaScriptNewFeature/083canvas绘制文字.md',
+                '/typora/01frontEnd/05JavaScriptNewFeature/084canvas绘制图片.md',
+                '/typora/01frontEnd/05JavaScriptNewFeature/085canvas帧动画1.md',
+                '/typora/01frontEnd/05JavaScriptNewFeature/086canvas帧动画2.md',
+                '/typora/01frontEnd/05JavaScriptNewFeature/087canvas帧动画3.md',
+                '/typora/01frontEnd/05JavaScriptNewFeature/088canvas形变.md',
+                '/typora/01frontEnd/05JavaScriptNewFeature/089canvas事件监听.md',
+                '/typora/01frontEnd/05JavaScriptNewFeature/090canvas炫彩小球.md',
+
+            ]
+        },
+        {
+            text: "7:SVG",
+            collapsable: true,
+            children:
+            [
+                '/typora/01frontEnd/05JavaScriptNewFeature/091SVG全部方法.md',
+                '/typora/01frontEnd/05JavaScriptNewFeature/092svg开篇.md',
+                '/typora/01frontEnd/05JavaScriptNewFeature/093svg使用方式.md',
+                '/typora/01frontEnd/05JavaScriptNewFeature/094svg绘制矩形.md',
+                '/typora/01frontEnd/05JavaScriptNewFeature/095svg绘制圆和椭圆.md',
+                '/typora/01frontEnd/05JavaScriptNewFeature/096svg绘制直线和折线.md',
+                '/typora/01frontEnd/05JavaScriptNewFeature/097svg常见属性.md',
+                '/typora/01frontEnd/05JavaScriptNewFeature/098svg绘制路径.md',
+                '/typora/01frontEnd/05JavaScriptNewFeature/099svg绘制圆弧.md',
+                '/typora/01frontEnd/05JavaScriptNewFeature/100svg绘制贝塞尔曲线.md',
+                '/typora/01frontEnd/05JavaScriptNewFeature/101svg文本绘制.md',
+                '/typora/01frontEnd/05JavaScriptNewFeature/102svg绘制路径文本.md',
+                '/typora/01frontEnd/05JavaScriptNewFeature/103svg绘制超链接.md',
+                '/typora/01frontEnd/05JavaScriptNewFeature/104svg绘制图片.md',
+                '/typora/01frontEnd/05JavaScriptNewFeature/105svg结构元素.md',
+                '/typora/01frontEnd/05JavaScriptNewFeature/106svg裁剪和蒙版.md',
+                '/typora/01frontEnd/05JavaScriptNewFeature/107svg渐变.md',
+                '/typora/01frontEnd/05JavaScriptNewFeature/108svg画笔.md',
+                '/typora/01frontEnd/05JavaScriptNewFeature/109svg形变.md',
+                '/typora/01frontEnd/05JavaScriptNewFeature/110view-box上.md',
+                '/typora/01frontEnd/05JavaScriptNewFeature/111viewbox下.md',
+                '/typora/01frontEnd/05JavaScriptNewFeature/112svg动画.md',
+                '/typora/01frontEnd/05JavaScriptNewFeature/113svg常用动画属性.md',
+                '/typora/01frontEnd/05JavaScriptNewFeature/114svg混合动画.md',
+                '/typora/01frontEnd/05JavaScriptNewFeature/115svg形变动画.md',
+                '/typora/01frontEnd/05JavaScriptNewFeature/116svg路径动画.md',
+                '/typora/01frontEnd/05JavaScriptNewFeature/117svg脚本编程.md',
+
+            ]
+        },
+        {
+            text: "8:Echarts",
+            collapsable: true,
+            children:
+            [
+                '/typora/01frontEnd/05JavaScriptNewFeature/118ECharts简介.md',
+                '/typora/01frontEnd/05JavaScriptNewFeature/119ECharts基本使用.md',
+                '/typora/01frontEnd/05JavaScriptNewFeature/120ECharts标题组件.md',
+                '/typora/01frontEnd/05JavaScriptNewFeature/121ECharts工具箱组件.md',
+                '/typora/01frontEnd/05JavaScriptNewFeature/122ECharts弹窗组件.md',
+                '/typora/01frontEnd/05JavaScriptNewFeature/123ECharts饼状图.md',
+                '/typora/01frontEnd/05JavaScriptNewFeature/124ECharts航线图.md'
+
+            ]
+        },
+    ]
